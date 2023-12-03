0abddd0c7b06f0e1d557f17c03c108b3962d7f44
Yetalit <shadowclash02@gmail.com>
2023-12-03 20:40:57 +0300

    Proje Baslangic

---
+++ UcakWebProje/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UcakWebProje.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
